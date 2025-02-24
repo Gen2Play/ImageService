3a8b024dd20af93080feb9882fcfd40ad012a6c3
ToanDC28 <toandoan2804@gmail.com>
2025-02-24 23:11:09 +0700

    Update Program.cs

--- Host/Program.cs
+++ Host/Program.cs
@@ -25,11 +25,8 @@ app.Services.InitializeDatabasesAsync().Wait();
 app.UseInFrastructure();
 Console.WriteLine($"Application Name: {app.Environment.ApplicationName}");
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHealthChecks("/health");
 
